1b2325d7ad9c7a62ce5e2d2e0c72b7fc4f483de5
benscott <ben@benscott.co.uk>
2025-02-04 13:38:46 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,6 @@
+#!/usr/bin/env python
 
-from setuptools import find_packages, setup
+import setuptools
 
-setup(
-    name="pkb",
-    version="0.1",
-    description='pkb',
-    # url='http://github.com/benscott/pkb',
-    author='Ben Scott',
-    author_email='ben@benscott.co.uk',    
-    packages=["pkb"],
-    entry_points={
-        'console_scripts': [
-            # 'pkb = pkb.cli:cli',
-        ],
-    },
-
-
-)
+if __name__ == "__main__":
+    setuptools.setup()
